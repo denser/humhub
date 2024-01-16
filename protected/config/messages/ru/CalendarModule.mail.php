83c659f7dd988ae6ec16d69edda6c1a46cfabc61
denser <denser.ru@gmail.com>
2024-01-16 09:47:29 +0700

    перевод в календаре

---
+++ protected/config/messages/ru/CalendarModule.mail.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    '<strong>Starting</strong> {date}' => '<strong>Назаначено на</strong> {date}',
+    'Additional information:' => 'Допольнитеьная информация:',
+    'Location:' => 'Место:',
+    'Organized by {userName}' => 'Организатор: {userName}',
+    'View Online: {url}' => 'Посмотреть: {url}',
+];
